2137c33af8e7943e77c6b2bc9e0eeb85029d9b61
Amit Tiwari <68313141+amit-omaze@users.noreply.github.com>
2021-12-01 14:16:53 +0530

    DATA2-689 Update convert_timezone
    * updated the macro to be compatible with snowflake. it gives error when we try to convert timestamp with tz with source and target tz

--- macros/calendar_date/convert_timezone.sql
+++ macros/calendar_date/convert_timezone.sql
@@ -1,5 +1,4 @@
 {%- macro convert_timezone(column, target_tz=None, source_tz=None) -%}
-{%- set source_tz = "UTC" if not source_tz else source_tz -%}
 {%- set target_tz = var("dbt_date:time_zone") if not target_tz else target_tz -%}
 {{ adapter.dispatch('convert_timezone', 'dbt_date') (column, target_tz, source_tz) }}
 {%- endmacro -%}
